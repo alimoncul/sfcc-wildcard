9ca4a403f5a329510b8c2b5637654093be994081
alimoncul <alimoncul@gmail.com>
2024-07-01 21:22:41 +0300

    feature: calculate highest order cartridge with cartridge path

--- src/definitionProvider.ts
+++ src/definitionProvider.ts
@@ -27,7 +27,7 @@ export class ImportDefinitionProvider implements vscode.DefinitionProvider {
         }
 
         const resolvedPath = this.resolveImportPath(importPath);
-        if (resolvedPath && fs.existsSync(resolvedPath)) {
+        if (resolvedPath) {
             // Find the start and end positions of the import path in the line
             const startPos = line.indexOf(importPathMatch[0]) + importPathMatch[0].indexOf(importPath);
             const endPos = startPos + importPath.length;
@@ -37,7 +37,7 @@ export class ImportDefinitionProvider implements vscode.DefinitionProvider {
                 new vscode.Position(position.line, startPos),
                 new vscode.Position(position.line, endPos)
             );
-            
+
             const targetRange = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, 0));
 
             return [
@@ -47,18 +47,31 @@ export class ImportDefinitionProvider implements vscode.DefinitionProvider {
                     targetRange: targetRange
                 }
             ];
-        } else {
-            console.log("Resolved path does not exist:", resolvedPath);
         }
     }
 
     private resolveImportPath(importPath: string): string {
         const basePath = vscode.workspace.workspaceFolders?.[0]?.uri?.fsPath || ''; // Get the workspace root path
-        const withCartridges = path.join(basePath, "cartridges", "int_adyen_SFRA");
-        const relativePath = importPath.replace('*/', '');
-        const resolvedPath = path.join(withCartridges, relativePath + '.js'); // Adjust the extension if necessary
-        console.log("Resolving path:", resolvedPath);
-        cartridgePathFinder();
-        return resolvedPath;
+        const cartridgePath = cartridgePathFinder();
+        const cartridges = cartridgePath?.split(":");
+        if(cartridges?.length) {
+            for(let i = 0; i < cartridges.length; i += 1) {
+                const cartridge = cartridges[i];
+                const withCartridges = path.join(basePath, "cartridges", cartridge);
+                const relativePath = importPath.replace('*/', '');
+                const resolvedPath = path.join(withCartridges, relativePath + '.js'); // Adjust the extension if necessary
+
+                if(fs.existsSync(resolvedPath)) {
+                    console.log("Resolving path:", resolvedPath);
+                    return resolvedPath;
+                }
+            }
+            
+            console.info("Relative file could not found in the cartridge path.");
+            return "";
+        } else {
+            console.warn("Cartridge path is not set, extension will not work!");
+            return "";
+        }
     }
 }
